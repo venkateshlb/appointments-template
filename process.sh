0725e5acb99791860d684462057d4ec92ac86e80
venkateshlb <venkatesh.babu@emishealth.com>
2020-02-19 12:14:33 +0530

    changes

--- process.sh
+++ process.sh
@@ -15,13 +15,13 @@ then
 fi
 
 ## Create
-sed 's/CreateSession/'$templateName'/g' template/create-session/lambda.go > $templateName/create-$templateName/lambda.go
+sed 's/CreateSession/Create'$templateName'/g' template/create-session/lambda.go > $templateName/create-$templateName/lambda.go
 
 ## Update
-sed 's/UpdateSession/'$templateName'/g' template/update-session/lambda.go > $templateName/update-$templateName/lambda.go
+sed 's/UpdateSession/Update'$templateName'/g' template/update-session/lambda.go > $templateName/update-$templateName/lambda.go
 
 ## Delete
-sed 's/DeleteSession/'$templateName'/g' template/delete-session/lambda.go > $templateName/delete-$templateName/lambda.go
+sed 's/DeleteSession/Delete'$templateName'/g' template/delete-session/lambda.go > $templateName/delete-$templateName/lambda.go
 
 ## Get
-sed 's/DeleteSession/'$templateName'/g' template/get-session/lambda.go > $templateName/get-$templateName/lambda.go
+sed 's/DeleteSession/Get'$templateName'/g' template/get-session/lambda.go > $templateName/get-$templateName/lambda.go
